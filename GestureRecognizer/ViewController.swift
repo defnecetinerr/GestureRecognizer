7bc8018dc17bcc30a6fa0c26a3db7019d1e4ed94
Defne Cetiner <defnecetiner00@gmail.com>
2022-07-03 17:18:48 +0300

    Initial Commit

---
+++ GestureRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GestureRecognizer
+//
+//  Created by Defne Çetiner on 3.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
